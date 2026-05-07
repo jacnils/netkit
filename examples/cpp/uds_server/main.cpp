d70f4b91b6063c0da1c4f6016b88a37b10ca0c8c
Jacob Nilsson <166003882+jacnils@users.noreply.github.com>
2026-05-07 14:24:22 +0200

    update example

--- examples/cpp/uds_server/main.cpp
+++ examples/cpp/uds_server/main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <string_view>
 #include <netkit/netkit.hpp>
 
 int main() {
+	std::filesystem::remove("/tmp/test.sock");
+	
     netkit::sock::addr addr("/tmp/test.sock");
-    netkit::sock::sync_sock sock(addr, netkit::sock::type::unix);
+    netkit::sock::sync_sock sock(addr, netkit::sock::type::uds);
 
     sock.bind();
     sock.listen();
     
     while (true) {
     	auto rec = sock.accept();
-	auto buffer = rec->recv().data;
-	std::cout << buffer << "\n";
+		auto buffer = rec->recv().data;
+		std::cout << buffer << "\n";
     }
 
     return 0;
